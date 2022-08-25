2ae1bc90a32424dafc30c6ebccced97157a9373b
DOMEMY <konton07@hotmail.com>
2022-08-25 21:51:57 +0700

    Updated

---
+++ src/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav';
+import Title from './Title';
+
+
+const Header = () => {
+    return (
+        <header>
+           <Nav></Nav>
+            <Title></Title>          
+        </header>
+
+
+    )
+}
+
+export default Header;
+
---
+++ src/Title.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './App.css';
+
+
+
+const Title = () => {
+
+    return (
+        <div className='title'>
+            <div className="header">
+                <h1>A modern publishing platform</h1>
+                Grow your audience and build your online brand
+                <div className="header-btn">
+                    <button className='startbtn'>Start for Free</button>
+                    <button className='learnbtn'>Learn More</button>
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Title
