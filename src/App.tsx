13bc416ce034e28b992107d7ebb8f27ad7540de3
DOMEMY <konton07@hotmail.com>
2022-09-01 22:40:59 +0700

    Adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './components/App.css';
 import Header from './components/Header';
 import Content from './components/Content/Content';
-
+import Information from './components/Content/Information';
+       
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Content></Content>
+      <Information></Information>
     </div>
   );
 }
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,12 +3,14 @@ import FirstContent from './FirstContent'
 import SecondContent from './SecondContent'
 import ThridContent from './ThridContent'
 
+
 const Content = () => {
     return (
         <div className="content">
            <FirstContent></FirstContent>
            <SecondContent></SecondContent>
            <ThridContent></ThridContent>
+
         </div>
     )
 
